e950988faebcf9e62310fd5e23ef83da769f27a4
Evgeniy <vigi73@mail.ru>
2017-02-26 19:49:51 +0700

    3

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from libs import pars
 from inn_org import inns
 from organization import  get_value
+from print_ import get_print, print_main
 
 
 
@@ -23,12 +24,15 @@ def get_pages(urls):
 
 
 if __name__ == '__main__':
+
+    print_main()
+
     st_time = datetime.now()
     driver = webdriver.Chrome()
     driver.set_window_size(0, 0)
     driver.set_window_position(1000, 0)
 
-    for inn in inns:
+    for i, inn in enumerate(inns, start=1):
         value1, value2, name_org = get_value(inn)
 
         urls = [f'http://bus.gov.ru/pub/agency/{value1}/tasks/{value2}',
@@ -39,12 +43,11 @@ if __name__ == '__main__':
                 f'http://bus.gov.ru/pub/agency/{value1}/annual-balances-f0503737',
                 f'http://bus.gov.ru/pub/agency/{value1}/reports',
                 f'http://bus.gov.ru/pub/agency/{value1}/measures'
-
                 ]
 
 
         result = get_pages(urls)
-        print(name_org, result)
+        get_print(i, name_org, result)
 
     driver.quit()
     end_time = datetime.now()
--- print_.py
+++ print_.py
@@ -1,7 +1,18 @@
+from datetime import datetime
+
+
 def get_print(i, org, lst):
     l = [i for i in lst if i != '-']
     pr = sum(l) * 100 / 8
-    with open('otchet.txt', 'a') as f:
+    with open('otchet.txt', 'a', encoding='utf-8') as f:
         print('-' * 127, file=f)
         print(f'|{i:^{5}}|{org:^{65}}|{lst[0]:^{5}}|{lst[1]:^{5}}|{lst[2]:^{5}}|{lst[3]:^{5}}|{lst[4]:^{5}}|{lst[5]:^{5}}|'
               f'{lst[6]:^{5}}|{lst[7]:^{5}}|{pr:^{5}}|', file=f)
+
+
+def print_main():
+    with open('otchet.txt', 'w', encoding='utf-8') as f:
+        print(datetime.today().now(), file=f)
+        print('-' * 127, file=f)
+        print(f'|{"№":^{5}}|{"Наименование организации":^{65}}|{"ГМЗ":^{5}}|{"ПФХД":^{5}}|{"ЦСБ":^{5}}|{"OФР":^{5}}|'
+              f'{"БГУ":^{5}}|{"ИУП":^{5}}|{"ИРД":^{5}}|{"СКМ":^{5}}|{"%":^{5}}|', file=f)
