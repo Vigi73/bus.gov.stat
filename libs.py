7b613d8408f5289039e78fa78fe72ec253ba4d53
Evgeniy <vigi73@mail.ru>
2017-02-26 19:06:33 +0700

    2

--- libs.py
+++ libs.py
@@ -1,30 +1,25 @@
-from selenium import webdriver
+
 from bs4 import BeautifulSoup as BS
 
 Y = '2017'
 
-def get_page_html(url):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    html = driver.page_source
-    driver.quit()
-    return html
 
 def pars(html):
     soup = BS(html, 'lxml')
     try:
-        r = soup.find('div', class_='info-date ng-binding ng-scope').text.strip()
+        r = soup.find('div', class_='info-container info-container_np').find('div', class_='info-date ng-binding ng-scope').text.strip()
         return 1 if r.split('.')[-1] == Y else 0
     except:
         try:
-            r = soup.find('div', class_='ng-binding ng-scope').text.strip()
+            r = soup.find('div', class_='info-date').find('span', class_='ng-binding ng-scope').text.strip()
             return 1 if r.split('.')[-1] == Y else 0
         except:
-            try:
-                r = soup.find('div', class_='info-date').text.strip()
-                return 1 if r.split('.')[-1] == Y else 0
-            except:
-                return '-'
+            return '-'
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # 2017 Alti, Kurya (c)Vigi-Soft
 #------------------------------
 from selenium import webdriver
-from bs4 import BeautifulSoup as BS
 from datetime import datetime
+from libs import pars
+from inn_org import inns
+from organization import  get_value
 
 
 
 def get_pages(urls):
-    st = datetime.now()
-    driver = webdriver.Chrome()
-    driver.set_window_size(0, 0)
-    driver.set_window_position(1000, 0)
+
+    list_answer = []
 
     for url in urls:
         driver.get(url)
@@ -18,33 +18,34 @@ def get_pages(urls):
             req = pars(driver.page_source)
             if req != '-':
                 break
-        print(req)
-    driver.quit()
-    en = datetime.now()
-    print(f'Using Time:{str(en - st)}')
-
-
-def pars(html):
-    soup = BS(html, 'lxml')
-    try:
-        r = soup.find('div', class_='info-container info-container_np').find('div', class_='info-date ng-binding ng-scope').text.strip()
-        return r
-    except:
-        try:
-            r = soup.find('div', class_='info-date').find('span', class_='ng-binding ng-scope').text.strip()
-            return r
-        except:
-            return '-'
+        list_answer.append(req)
+    return list_answer
 
 
 if __name__ == '__main__':
-    urls = ['http://bus.gov.ru/pub/agency/206030/tasks/3573224',
-            'http://bus.gov.ru/pub/agency/206030/plans',
-            'http://bus.gov.ru/pub/agency/206030/operations/',
-            'http://bus.gov.ru/pub/agency/206030/annual-balances-F0503721',
-            'http://bus.gov.ru/pub/agency/206030/annual-balances-F0503730',
-            'http://bus.gov.ru/pub/agency/206030/annual-balances-f0503737',
-            'http://bus.gov.ru/pub/agency/206030/reports',
-            'http://bus.gov.ru/pub/agency/206030/measures'
-            ]
-    get_pages(urls)
+    st_time = datetime.now()
+    driver = webdriver.Chrome()
+    driver.set_window_size(0, 0)
+    driver.set_window_position(1000, 0)
+
+    for inn in inns:
+        value1, value2, name_org = get_value(inn)
+
+        urls = [f'http://bus.gov.ru/pub/agency/{value1}/tasks/{value2}',
+                f'http://bus.gov.ru/pub/agency/{value1}/plans',
+                f'http://bus.gov.ru/pub/agency/{value1}/operations/',
+                f'http://bus.gov.ru/pub/agency/{value1}/annual-balances-F0503721',
+                f'http://bus.gov.ru/pub/agency/{value1}/annual-balances-F0503730',
+                f'http://bus.gov.ru/pub/agency/{value1}/annual-balances-f0503737',
+                f'http://bus.gov.ru/pub/agency/{value1}/reports',
+                f'http://bus.gov.ru/pub/agency/{value1}/measures'
+
+                ]
+
+
+        result = get_pages(urls)
+        print(name_org, result)
+
+    driver.quit()
+    end_time = datetime.now()
+    print(str(end_time - st_time))
